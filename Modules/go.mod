2d19e59960bcc28123e98b25701b5a550332cc81
vashu992 <ashutoshpratap992@gmail.com>
2024-04-05 22:51:51 +0530

    Modules

---
+++ Modules/go.mod
@@ -0,0 +1,3 @@
+module github.com/vashu992/Restart/Modules
+
+go 1.22.0
---
+++ Modules/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("You can not imoprt two package as main and non main under same folder")
+	fmt.Println("core library and third party library as fmt and gorrilla mux")
+	fmt. Println(" if not import any third party in folder then you can run vendor")
+}
+
+
